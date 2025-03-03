8a6955a80e2770136ac8bccee25a29180cf4c797
Luana Silva <luana.dossantos@zup.com.br>
2025-03-03 19:03:53 -0300

    feat: adds JwtTokenProvider to the project

--- src/main/java/com/catalisa/desafio_imposto/infra/jwt/JwtTokenProvider.java
+++ src/main/java/com/catalisa/desafio_imposto/infra/jwt/JwtTokenProvider.java
@@ -1,4 +1,69 @@
 package com.catalisa.desafio_imposto.infra.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
 public class JwtTokenProvider {
+    private String jwtSecret = "YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGZhc2Rm";
+    private long jwtExpirationDate = 300000; // 5 minutes
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+
+        // Extract roles from the authentication object
+        String roles = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        // Add custom claim "department"
+        String token = Jwts.builder()
+                .setSubject(username)
+                .claim("roles", roles)
+                .setIssuedAt(currentDate)
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS256, key())
+                .compact();
+
+        return token;
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+    // Validate JWT token
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
 }
