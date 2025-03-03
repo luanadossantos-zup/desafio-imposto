9d767d6287777994f4c4163448671e311498b2b2
Luana Silva <luana.dossantos@zup.com.br>
2025-03-03 09:37:46 -0300

    chore: setting up the repository for the first time

---
+++ src/main/java/com/catalisa/desafio_imposto/DesafioImpostoApplication.java
@@ -0,0 +1,13 @@
+package com.catalisa.desafio_imposto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioImpostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioImpostoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalisa/desafio_imposto/DesafioImpostoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalisa.desafio_imposto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioImpostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
