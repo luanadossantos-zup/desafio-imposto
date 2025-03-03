c9478208462c0a88d2974e1067c71e240d817ea4
Luana Silva <luana.dossantos@zup.com.br>
2025-03-03 19:06:27 -0300

    feat: adds dtos RegisterUser and Login to the project

--- src/main/java/com/catalisa/desafio_imposto/dto/LoginDto.java
+++ src/main/java/com/catalisa/desafio_imposto/dto/LoginDto.java
@@ -1,4 +1,22 @@
 package com.catalisa.desafio_imposto.dto;
 
 public class LoginDto {
+    private String username;
+    private String password;
+
+    public LoginDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+
 }
--- src/main/java/com/catalisa/desafio_imposto/dto/RegisterUserDto.java
+++ src/main/java/com/catalisa/desafio_imposto/dto/RegisterUserDto.java
@@ -1,4 +1,26 @@
 package com.catalisa.desafio_imposto.dto;
 
+import com.catalisa.desafio_imposto.model.Roles;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({ "username", "role", "id"})
 public class RegisterUserDto {
+
+    private String username;
+    private String password;
+    private Roles role;
+
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Roles getRole() {
+        return role;
+    }
 }
