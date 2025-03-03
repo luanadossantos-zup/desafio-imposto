7f0d32b5954b84942c7460b5968031f44bea9cee
Luana Silva <luana.dossantos@zup.com.br>
2025-03-03 15:00:56 -0300

    feat: adds Role and Usuario repositories

--- src/main/java/com/catalisa/desafio_imposto/repository/RoleRepository.java
+++ src/main/java/com/catalisa/desafio_imposto/repository/RoleRepository.java
@@ -1,4 +1,7 @@
 package com.catalisa.desafio_imposto.repository;
 
-public interface RoleRepository {
+import com.catalisa.desafio_imposto.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
 }
--- src/main/java/com/catalisa/desafio_imposto/repository/UsuarioRepository.java
+++ src/main/java/com/catalisa/desafio_imposto/repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package com.catalisa.desafio_imposto.repository;
 
-public interface UsuarioRepository {
+import com.catalisa.desafio_imposto.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
