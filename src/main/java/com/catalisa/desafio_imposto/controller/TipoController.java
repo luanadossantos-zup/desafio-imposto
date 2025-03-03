3ad4bbca667b893958ce82cdadfd360d8c628485
Luana Silva <luana.dossantos@zup.com.br>
2025-03-03 14:47:43 -0300

    feat: adds TipoController to the project

--- src/main/java/com/catalisa/desafio_imposto/controller/TipoController.java
+++ src/main/java/com/catalisa/desafio_imposto/controller/TipoController.java
@@ -1,4 +1,31 @@
 package com.catalisa.desafio_imposto.controller;
 
+import com.catalisa.desafio_imposto.dto.ImpostoDto;
+import com.catalisa.desafio_imposto.service.ImpostoService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/tipos")
 public class TipoController {
+
+    private final ImpostoService impostoService;
+
+    public TipoController(ImpostoService impostoService) {
+        this.impostoService = impostoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ImpostoDto>> listarTodosImpostos() {
+        return ResponseEntity.ok(impostoService.listarTodosImpostos());
+    }
+
+
 }
