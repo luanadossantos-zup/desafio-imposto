cc9e907eaccec914fb5edc304b91b9074524be62
Luana Silva <luana.dossantos@zup.com.br>
2025-03-04 15:09:06 -0300

    refactor: removes methods from Enum class

--- src/main/java/com/catalisa/desafio_imposto/model/TipoImposto.java
+++ src/main/java/com/catalisa/desafio_imposto/model/TipoImposto.java
@@ -1,23 +1,7 @@
 package com.catalisa.desafio_imposto.model;
 
-public enum TipoImposto implements CalculoImposto{
-    ICMS {
-        @Override
-        public double calcular(Double valorBase, Double aliquota) {
-            return valorBase * (aliquota / 100);
-        }
-    },
-    ISS {
-        @Override
-        public double calcular(Double valorBase, Double aliquota) {
-            return valorBase * (aliquota / 100);
-        }
-    },
-    IPI {
-        @Override
-        public double calcular(Double valorBase, Double aliquota) {
-            return valorBase * (aliquota / 100);
-        }
-    };
-
+public enum TipoImposto {
+    ICMS,
+    ISS,
+    IPI;
 }
