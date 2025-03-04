40f04721ac82fc4758d8fbb7097ba77ab29995fe
Luana Silva <luana.dossantos@zup.com.br>
2025-03-04 15:28:54 -0300

    feat: adds api cadastrarNovoImposto and delete imposto

--- src/main/java/com/catalisa/desafio_imposto/controller/TipoController.java
+++ src/main/java/com/catalisa/desafio_imposto/controller/TipoController.java
@@ -39,5 +39,32 @@ public class TipoController {
         return ResponseEntity.ok(impostoDtos);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public ResponseEntity<ImpostoDto> cadastrarNovoImposto(@RequestBody ImpostoDto impostoDto) {
+
+        Imposto imposto = new Imposto();
+        imposto.setNome(impostoDto.getNome());
+        imposto.setDescricao(impostoDto.getDescricao());
+        imposto.setAliquota(impostoDto.getAliquota());
+
+        Imposto impostoSalvo = impostoServiceImpl.salvarImposto(imposto);
+
+        ImpostoDto impostoSalvoDto = new ImpostoDto(
+                impostoSalvo.getId(),
+                impostoSalvo.getNome(),
+                impostoSalvo.getDescricao(),
+                impostoSalvo.getAliquota()
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(impostoSalvoDto);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        impostoServiceImpl.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
