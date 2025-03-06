13fc9291ebc4fb6dc38d00f24f2293e1f095ead9
Luana Silva <luana.dossantos@zup.com.br>
2025-03-06 14:17:59 -0300

    feat: creates a test class for UsuarioServiceImpl

--- src/test/java/com/catalisa/desafio_imposto/service/UsuarioServiceImplTest.java
+++ src/test/java/com/catalisa/desafio_imposto/service/UsuarioServiceImplTest.java
@@ -1,5 +1,21 @@
 package com.catalisa.desafio_imposto.service;
 
+import com.catalisa.desafio_imposto.dto.RegisterUserDto;
+import com.catalisa.desafio_imposto.model.Roles;
+import com.catalisa.desafio_imposto.model.Usuario;
+import com.catalisa.desafio_imposto.repository.UsuarioRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class UsuarioServiceImplTest {
 
 
