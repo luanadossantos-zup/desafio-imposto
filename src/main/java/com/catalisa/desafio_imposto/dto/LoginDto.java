d18b05e585b6c8e9fb24c18de043f29cf4773e53
Luana Silva <luana.dossantos@zup.com.br>
2025-03-10 09:57:19 -0300

    feat: adds set Username and Password to LoginDto

--- src/main/java/com/catalisa/desafio_imposto/dto/LoginDto.java
+++ src/main/java/com/catalisa/desafio_imposto/dto/LoginDto.java
@@ -4,8 +4,18 @@ public class LoginDto {
     private String username;
     private String password;
 
+
+
     public LoginDto(String username, String password) {
         this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 
--- src/test/java/com/catalisa/desafio_imposto/service/CustomUserDetailsServiceImplTest.java
+++ src/test/java/com/catalisa/desafio_imposto/service/CustomUserDetailsServiceImplTest.java
@@ -1,4 +1,63 @@
+package com.catalisa.desafio_imposto.service;
+
+import com.catalisa.desafio_imposto.model.Roles;
+import com.catalisa.desafio_imposto.model.Usuario;
+import com.catalisa.desafio_imposto.repository.UsuarioRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class CustomUserDetailsServiceImplTest {
-  
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @InjectMocks
+    private CustomUserDetailsServiceImpl customUserDetailsService;
+
+    @Test
+    void loadUserByUsername_Success() {
+        String username = "testUser";
+        String password = "testPassword";
+        Roles role = Roles.ROLE_USER;
+
+        Usuario mockUsuario = new Usuario();
+        mockUsuario.setUsername(username);
+        mockUsuario.setPassword(password);
+        mockUsuario.setRole(role);
+
+        when(usuarioRepository.findByUsername(username)).thenReturn(Optional.of(mockUsuario));
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+        assertEquals(username, userDetails.getUsername());
+        assertEquals(password, userDetails.getPassword());
+        assertEquals(1, userDetails.getAuthorities().size());
+        assertEquals("ROLE_USER", userDetails.getAuthorities().iterator().next().getAuthority());
+
+        verify(usuarioRepository, times(1)).findByUsername(username);
+    }
+
+    @Test
+    public void testLoadUserByUsername_UserNotFound() {
+
+        String username = "nonExistentUser";
+        when(usuarioRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+        assertThrows(UsernameNotFoundException.class, () -> {
+            customUserDetailsService.loadUserByUsername(username);
+        });
+
+        verify(usuarioRepository, times(1)).findByUsername(username);
+    }
 }
