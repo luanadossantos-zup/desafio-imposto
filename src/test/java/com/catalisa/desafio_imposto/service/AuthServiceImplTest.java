24f6b180f4cb6e57d648f28c5852caba3d0c3106
Luana Silva <luana.dossantos@zup.com.br>
2025-03-10 11:32:37 -0300

    feat: adds testLoginSuccess

--- src/test/java/com/catalisa/desafio_imposto/service/AuthServiceImplTest.java
+++ src/test/java/com/catalisa/desafio_imposto/service/AuthServiceImplTest.java
@@ -1,4 +1,46 @@
 import static org.junit.jupiter.api.Assertions.*;
 class AuthServiceImplTest {
-  
+
+    @InjectMocks
+    private AuthServiceImpl authServiceImpl;
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @Mock
+    private Authentication authentication;
+
+
+    @Test
+    void testLoginSuccess() {
+        // Arrange
+        LoginDto loginDto = new LoginDto("testUser","testPassword");
+
+        Usuario usuario = new Usuario();
+        usuario.setUsername("testUser");
+
+        Mockito.when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(authentication);
+        Mockito.when(usuarioRepository.findByUsername("testUser"))
+                .thenReturn(Optional.of(usuario));
+        Mockito.when(jwtTokenProvider.generateToken(authentication))
+                .thenReturn("mockedToken");
+
+        // Act
+        String token = authServiceImpl.login(loginDto);
+
+        // Assert
+        Mockito.verify(authenticationManager).authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class));
+        Mockito.verify(usuarioRepository).findByUsername("testUser");
+        Mockito.verify(jwtTokenProvider).generateToken(authentication);
+        Assertions.assertEquals("mockedToken", token, "O token gerado deve ser igual ao esperado.");
+    }
+
+    
 }
