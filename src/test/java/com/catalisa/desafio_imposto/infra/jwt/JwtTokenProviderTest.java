6b84d70395d4dfe7ccb4b888b442671c4e148dbd
Luana Silva <luana.dossantos@zup.com.br>
2025-03-12 16:03:46 -0300

    feat: add JwtTokenProvider test to the project

---
+++ src/test/java/com/catalisa/desafio_imposto/infra/jwt/JwtTokenProviderTest.java
@@ -0,0 +1,80 @@
+package com.catalisa.desafio_imposto.infra.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class JwtTokenProviderTest {
+
+    @InjectMocks
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Mock
+    private Authentication authentication;
+
+    @Test
+    void generateToken_DeveGerarTokenValido() {
+        String username = "testUser";
+        Collection<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+
+        Mockito.when(authentication.getName()).thenReturn(username);
+        Mockito.when(authentication.getAuthorities()).thenAnswer(invocation -> authorities);
+
+        String token = jwtTokenProvider.generateToken(authentication);
+
+        assertNotNull(token);
+        assertTrue(jwtTokenProvider.validateToken(token));
+        assertEquals(username, jwtTokenProvider.getUsername(token));
+    }
+
+    @Test
+    void getUsername_DeveRetornarUsernameCorreto() {
+        // Arrange
+        String username = "testUser";
+        Collection<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+
+        Mockito.when(authentication.getName()).thenReturn(username);
+        Mockito.when(authentication.getAuthorities()).thenAnswer(invocation -> authorities);
+
+        String token = jwtTokenProvider.generateToken(authentication);
+
+        // Act
+        String extractedUsername = jwtTokenProvider.getUsername(token);
+
+        // Assert
+        assertEquals(username, extractedUsername);
+    }
+
+    @Test
+    void validateToken_DeveRetornarTrueParaTokenValido() {
+        // Arrange
+        String username = "testUser";
+        Collection<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+
+        Mockito.when(authentication.getName()).thenReturn(username);
+        Mockito.when(authentication.getAuthorities()).thenAnswer(invocation -> authorities);
+
+        String token = jwtTokenProvider.generateToken(authentication);
+
+        // Act
+        boolean isValid = jwtTokenProvider.validateToken(token);
+
+        // Assert
+        assertTrue(isValid);
+    }
+
+
+
+}
