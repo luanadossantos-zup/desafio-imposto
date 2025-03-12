e4853a68996c3583ee0c692407eba212d692f492
Luana Silva <luana.dossantos@zup.com.br>
2025-03-12 15:16:11 -0300

    fix: add setId to Imposto

--- src/main/java/com/catalisa/desafio_imposto/model/Imposto.java
+++ src/main/java/com/catalisa/desafio_imposto/model/Imposto.java
@@ -35,6 +35,10 @@ public class Imposto {
         this.aliquota = aliquota;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Long getId() {
         return id;
     }
